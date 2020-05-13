0ebb46c1b286f1da472e1c1ff631a5772299db37
David Schnurr <schnurr@gmail.com>
2020-05-12 18:42:59 -0700

    tests passing

--- spec/ckmeans.common.spec.js
+++
@@ -1,9 +0,0 @@
-/* globals describe, it, expect */
-var ckmeans = require('../src/ckmeans');
-
-describe('Ckmeans', function () {
-  it('should find clusters', function () {
-    const result = ckmeans([1, 2, 4, 5, 12, 43, 52, 123, 234, 1244], 6);
-    expect(result).toEqual([1, 12, 43, 123, 234, 1244]);
-  });
-});
